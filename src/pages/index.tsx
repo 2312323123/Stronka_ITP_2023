2ad60a791f47da07b3254b1ac180cc62582ad265
Darnok00 <56363711+Darnok00@users.noreply.github.com>
2023-02-22 19:57:02 +0100

    Add sponsors and organizers
    * create sponsors components without logos and animations
    
    * sponsors and organizers works without animations
    
    * add map component
    
    * create sponsors components without logos and animations
    
    * resolve conflicts
    
    * resolve conflicts
    
    * small layout fix
    
    ---------
    
    Co-authored-by: pawelzareba <pzareba22@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,25 @@ import Layout from '../components/Layout'
 // import global styles
 import '../styles/global.scss'
 import TitlePage from '../components/TitlePage'
+import Map from '../components/Map'
+import Sponsors from '../components/Sponsors'
+import Organizers from '../components/Organizers'
 
 const MainPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <TitlePage />
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam
-      dignissimos, dolor eaque eius et eveniet in inventore iste nobis, odit
-      possimus quaerat qui ratione recusandae rem vitae! At, blanditiis deserunt
-      ea eaque earum eligendi eum eveniet inventore ipsa iusto maxime neque
-      nobis nulla omnis quaerat repudiandae voluptas voluptatem voluptates.
+      <Map />
+      <Sponsors />
+      <Organizers />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+        aperiam dignissimos, dolor eaque eius et eveniet in inventore iste
+        nobis, odit possimus quaerat qui ratione recusandae rem vitae! At,
+        blanditiis deserunt ea eaque earum eligendi eum eveniet inventore ipsa
+        iusto maxime neque nobis nulla omnis quaerat repudiandae voluptas
+        voluptatem voluptates.
+      </p>
     </Layout>
   )
 }
