9d8770cd7c80e19f93b74ad85a798ea1bc56ad4e
Konrad Krzeminski <majunia28@gmail.com>
2023-03-01 10:45:58 +0100

    map images chnaged

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,20 @@
 
 import React from 'react';
 //@ts-ignore
-import img3_desktop from '../images/DesktopBackgrounds/3.jpg';
+import img3_desktop from '../images/desktop_backgrounds/3.jpg';
 //@ts-ignore
-import img3_mobile from '../images/MobileBackgrounds/3.jpg';
+import img3_mobile from '../images/mobile_backgrounds/3.png';
 //@ts-ignore
-import map1 from '../images/map.svg';
 import styled from "styled-components";
+
+function importAll(r: any) {
+    return r.keys().map(r)
+  }
+  
+const mapImages = importAll(
+    require.context('../images/map', false, /\.(png|jpe?g|svg)$/)
+)
+
 const Img = styled.img`
   width: 100%;
   height: 100%;
@@ -20,32 +28,60 @@ const Container = styled.div`
 
 const MapImg = styled.img`
     position: absolute;
-    top: 18%;
+    top: 2%;
+    left: 3%;
+    width: 94%;
     @media (max-width: 769px) {
        width: 95%;
-       top: 25%;
+       top: 15%;
     }
 `
 
-const MapBox = styled.div`
-  width: 80%;
-  height: 60%;
-  position: absolute;
-  left: 10%;
-  top: 20%;
-  background-color: #e9d8cc;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  border 0.1rem solid;
-  border-radius 10rem;
-  @media (max-width: 769px) {
-    width: 92%;
-    height 27%;
-    top: 16%;
-    left: 4%;
-    border-radius 1rem;
-  };
+const List = styled.img`
+    position: absolute;
+    top: 64%;
+    width: 18%;
+    @media (max-width: 769px) {
+        top: 40%;
+        width: 25%;
+    }
+`
+
+const List1 = styled(List)`
+    left: 7%;
+    @media (max-width: 769px) {
+        top: 36%;
+        left: 4%;
+    }
+`
+
+const List2 = styled(List)`
+    left: 33.4%;
+    top: 67%;
+    width: 15%;
+    @media (max-width: 769px) {
+        left: 25%;
+        top: 48%;
+        width: 25%;
+    }
+`
+
+const List3 = styled(List2)`
+    left: 52%;
+    top: 65.4%;
+    width: 17%;
+    @media (max-width: 769px) {
+        left: 70%;
+        width: 28%;
+        top: 47%;
+    }
+`
+
+const List4 = styled(List1)`
+    left: 75.5%;
+    @media (max-width: 769px) {
+        left: 48%;
+    }
 `
 
 const Map: React.FC = () => {
@@ -57,9 +93,11 @@ const Map: React.FC = () => {
                     <source srcSet={img3_mobile} media="(max-width: 768px)" />
                     <Img src={img3_desktop} alt="last page"/>
                 </picture>
-                <MapBox>
-                    <MapImg src={map1} alt="map"/>
-                </MapBox>
+                <MapImg src={mapImages[0].default} alt="map"/>
+                <List1 src={mapImages[1].default} alt="list1"/>
+                <List2 src={mapImages[2].default} alt="list2"/>
+                <List3 src={mapImages[4].default} alt="list3"/>
+                <List4 src={mapImages[3].default} alt="list4"/>
             </Container>
         </>
 
