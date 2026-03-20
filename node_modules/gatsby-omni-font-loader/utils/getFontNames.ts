beac0c1a1d32db048333fe9b266ff89e064f3c3c
Maciej Piotrowski <maciej.piotrowski1@ibm.com>
2026-03-20 01:22:41 +0100

    Deploy with pathPrefix for GitHub Pages

--- node_modules/gatsby-omni-font-loader/utils/getFontNames.ts
+++
@@ -1,7 +0,0 @@
-export const getFontNames = (allFonts: { name: string }[]) => {
-  const fontNames = []
-  allFonts.forEach(({ name }) =>
-    Array.isArray(name) ? fontNames.push(...name) : fontNames.push(name)
-  )
-  return fontNames
-}
